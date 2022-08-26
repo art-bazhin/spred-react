1c3e4f204439f519f73ac7caa3b22c7202febb41
art-bazhin <bazhin.artyom@yandex.ru>
2022-08-26 13:45:08 +0300

    perf: use just one memo

--- src/useSignal/useSignal.ts
+++ src/useSignal/useSignal.ts
@@ -1,4 +1,4 @@
-import { DependencyList, useMemo, useCallback } from 'react';
+import { DependencyList, useMemo } from 'react';
 import { unstable_batchedUpdates } from 'react-dom';
 import { isSignal, Signal, configure } from 'spred';
 import { useSyncExternalStore } from 'use-sync-external-store/shim';
@@ -17,8 +17,9 @@ const EMPTY: DependencyList = [];
 
 export function useSignal<T>(
   signalFactory: () => Signal<T>,
-  deps: DependencyList
+  deps?: DependencyList
 ): T;
+
 export function useSignal<T>(signal: Signal<T>): T;
 
 export function useSignal<T>(
@@ -27,10 +28,11 @@ export function useSignal<T>(
 ) {
   const deps =
     arguments.length === 1 ? [signalOrFactory] : dependencies || EMPTY;
-  const signal = useMemo(() => getSignal(signalOrFactory), deps);
-  const subscribe = useCallback((cb) => signal.subscribe(cb, false), deps);
-  const getSnapshot = useCallback(() => signal.sample(), deps);
-  const value = useSyncExternalStore(subscribe, getSnapshot);
 
-  return value;
+  const args: [(cb: any) => () => any, () => T] = useMemo(() => {
+    const signal = getSignal(signalOrFactory);
+    return [(cb: any) => signal.subscribe(cb, false), () => signal.sample()];
+  }, deps);
+
+  return useSyncExternalStore(...args);
 }
