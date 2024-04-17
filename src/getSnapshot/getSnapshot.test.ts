984d929832575ceddaa0ec4fe68dbfff70a4cbd1
art-bazhin <bazhin.artyom@yandex.ru>
2024-04-17 16:05:07 +0300

    feat: add ability to use getSnapshot with objects containing non-signal properties

--- src/getSnapshot/getSnapshot.test.ts
+++ src/getSnapshot/getSnapshot.test.ts
@@ -15,4 +15,23 @@ describe('getSnapshot function', () => {
     expect(getSnapshot(input).a).toBe(1);
     expect(getSnapshot(input).b).toBe(2);
   });
+
+  it('copies non-signal properties', () => {
+    const a = signal(0);
+    const b = signal(() => a.get() * 2);
+    const fn = () => {};
+    const value = 1;
+
+    const input = { a, b, fn, value };
+
+    expect(getSnapshot(input).a).toBe(0);
+    expect(getSnapshot(input).b).toBe(0);
+
+    a.set(1);
+    expect(getSnapshot(input).a).toBe(1);
+    expect(getSnapshot(input).b).toBe(2);
+
+    expect(getSnapshot(input).fn).toBe(fn);
+    expect(getSnapshot(input).value).toBe(value);
+  });
 });
--- src/getSnapshot/getSnapshot.ts
+++ src/getSnapshot/getSnapshot.ts
@@ -1,17 +1,21 @@
-import { Signal } from '@spred/core';
+import { Signal, isSignal } from '@spred/core';
 
-export type Reactive<T> = {
-  [K in keyof T]: Signal<T[K]>;
+type Snapshot<T extends Record<any, unknown>> = {
+  [K in keyof T]: T[K] extends Signal<any> ? ReturnType<T[K]['get']> : T[K];
 };
 
 /**
- * Creates a snapshot of an object with reactive properties.
- * @param state An object with properties that are signals.
- * @returns An object with current values of state properties.
+ * Creates an object containing the current values of the source object signal properties. Values of non-signal properties are copied.
+ * @param state A source object.
+ * @returns Source object snapshot.
  */
-export function getSnapshot<T>(state: Reactive<T>) {
-  const result = {} as T;
+export function getSnapshot<T extends Record<any, unknown>>(state: T) {
+  const result = {} as Snapshot<T>;
 
-  for (let key in state) result[key] = state[key].get();
+  for (let key in state) {
+    const value = state[key];
+    (result[key] as any) = isSignal(value) ? value.get() : value;
+  }
+
   return result;
 }
